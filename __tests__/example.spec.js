69678fea3c9a63d255698cf5f690f59b6805a937
ksugimori <ksugimori.private@gmail.com>
2021-03-05 22:54:48 +0900

    setup Supertest

---
+++ __tests__/example.spec.js
@@ -0,0 +1,11 @@
+const request = require('supertest')
+const app = require('../app.js')
+
+describe('GET /', () => {
+  test('ルートページに Express という文字列が存在すること', async () => {
+    const response = await request(app).get('/')
+
+    expect(response.statusCode).toBe(200)
+    expect(response.text).toMatch(/Express/)
+  })
+});
