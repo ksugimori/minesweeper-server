d0dff91350e9ad9105c6c998304a73927aabccf9
ksugimori <12605906+ksugimori@users.noreply.github.com>
2021-03-20 22:57:33 +0900

    POST /games 作成

--- db/ddl/ddl_001_games.sql
+++ db/ddl/ddl_001_games.sql
@@ -2,11 +2,10 @@ CREATE TABLE games (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   width INT NOT NULL,
   height INT NOT NULL,
+  numMines INT NOT NULL,
+  startTime TIMESTAMP,
   status VARCHAR(10) DEFAULT 'INIT',
   mines JSON NOT NULL DEFAULT ('[]'),
   opens JSON NOT NULL DEFAULT ('[]'),
   flags JSON NOT NULL DEFAULT ('[]')
 );
-
-INSERT INTO games (width, height, status, mines) VALUES (2, 2, 'INIT', '[{"x": 0, "y": 0}, {"x": 1, "y": 1}]');
-INSERT INTO games (width, height, status, mines) VALUES (2, 2, 'INIT', '[{"x": 0, "y": 0}]');
--- routes/games.js
+++ routes/games.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const gameController = require('../controllers/GameController')
 
 /* Game の作成 */
-router.post('/', function (req, res) {
+router.post('/', async function (req, res) {
   // TODO 例外処理
 
-  const game = gameController.create(req.body.setting)
+  const game = await gameController.create(req.body.setting)
 
   // TODO ここ save した結果から mines を削除したもので良いかも
   res.status(201).json(game)
