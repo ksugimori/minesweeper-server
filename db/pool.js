0471417dc63f5d837ba4f95c02c4ce1a924d0c15
ksugimori <12605906+ksugimori@users.noreply.github.com>
2021-03-21 14:36:15 +0900

    DB接続情報を環境変数から取得するように変更

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2')
 // create the connection to database
 const pool = mysql.createPool({
   host: process.env.MS_DB_HOST,
-  user: 'ms-user',
-  password: 'ms-password',
-  database: 'minesweeper'
+  user: process.env.MS_DB_USER,
+  password: process.env.MS_DB_PASSWORD,
+  database: process.env.MS_DB_SCHEMA
 })
 
 module.exports = pool
