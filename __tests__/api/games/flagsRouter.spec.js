39143ab86b87c1eda9f14637bcf26c820ca573d7
ksugimori <12605906+ksugimori@users.noreply.github.com>
2021-04-10 11:14:09 +0900

    テスト用の Game 作成処理を共通化

--- __tests__/api/games/flagsRouter.spec.js
+++ __tests__/api/games/flagsRouter.spec.js
@@ -1,37 +1,15 @@
 const request = require('supertest')
 const app = require('../../../app.js')
-const Game = require('../../../models/Game')
-const random = require('../../../models/util/random')
 const Point = require('../../../models/util/Point')
 const gameRepository = require('../../../repositories/gameRepository')
 jest.mock('../../../repositories/gameRepository')
-
-/**
- * Game オブジェクトを初期化する。
- * @param {Number} width 幅
- * @param {Number} height 高さ
- * @param {Array} mines 地雷の座標
- */
-function initGame (width, height, ...mines) {
-  const game = new Game()
-
-  const numMines = mines.length
-  game.setting.merge({ width, height, numMines })
-
-  // ランダムな地雷の配置はモックする
-  const spy = jest.spyOn(random, 'points')
-  spy.mockReturnValue(mines)
-
-  game.initialize()
-
-  return game
-}
+const mockUtils = require('../../utils/mockUtils')
 
 describe('GET /api/games/:gameId/flags', () => {
   test('フラグの一覧が取得できること', async () => {
     // |1|*|2|
     // |1|2|*|
-    const game = initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
+    const game = mockUtils.initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
     game.id = 999
     game.open(0, 0)
 
@@ -51,7 +29,7 @@ describe('POST /api/games/:gameId/flags', () => {
   test('フラグが追加できること', async () => {
     // |1|*|2|
     // |1|2|*|
-    const game = initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
+    const game = mockUtils.initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
     game.id = 999
     game.open(0, 0)
     gameRepository.get = jest.fn(() => game)
@@ -69,7 +47,7 @@ describe('POST /api/games/:gameId/flags', () => {
   test('すでにフラグが存在する場合は 204 が返却されること', async () => {
     // |1|*|2|
     // |1|2|*|
-    const game = initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
+    const game = mockUtils.initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
     game.id = 999
     game.open(0, 0)
     game.flag(1, 0)
@@ -89,7 +67,7 @@ describe('DELETE /api/games/:gameId/flags/:id', () => {
   test('フラグが外されること', async () => {
     // |1|*|2|
     // |1|2|*|
-    const game = initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
+    const game = mockUtils.initGame(3, 2, Point.of(1, 0), Point.of(2, 1))
     game.id = 999
     game.open(0, 0)
 
