08aee9c51f2f390be8344972109b1b1e09f17428
ksugimori <ksugimori.private@gmail.com>
2021-04-10 21:59:20 +0900

    flags, opens のパスを /cells 以下に統一

---
+++ routes/games/cellsRouter.js
@@ -0,0 +1,108 @@
+const gameRepository = require('../../lib/repositories/gameRepository')
+const Point = require('../../lib/models/util/Point')
+
+/**
+ * flags へのルーティングを設定する。
+ * @param {Router} router games に対するルータ
+ */
+exports.route = function (router) {
+  /**
+   * フラグ一覧の取得
+   */
+  router.get('/:gameId/cells/flag', async function (req, res, next) {
+    try {
+      const game = await gameRepository.get(req.params.gameId)
+      const flags = game.field.points(cell => cell.isFlag)
+      res.status(200).json(flags)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  /**
+   * フラグを作成する
+   */
+  router.post('/:gameId/cells/flag', async function (req, res, next) {
+    try {
+      const game = await gameRepository.get(req.params.gameId)
+      const point = { x: req.body.x, y: req.body.y }
+
+      if (game.field.cellAt(point).isFlag) {
+        res.status(204).end()
+        return
+      }
+
+      game.flag(point.x, point.y)
+      await gameRepository.update(game)
+
+      res.status(201).json(point)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  /**
+   * フラグを削除する
+   */
+  router.delete('/:gameId/cells/flag/:id', async function (req, res, next) {
+    try {
+      const point = Point.parseId(req.params.id)
+      const game = await gameRepository.get(req.params.gameId)
+
+      const cell = game.field.cellAt(point)
+      if (cell.isFlag) {
+        cell.unflag()
+      }
+
+      await gameRepository.update(game)
+
+      res.status(204).end()
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  /**
+   * 開いたセル一覧の取得
+   */
+  router.get('/:gameId/cells/open', async function (req, res, next) {
+    try {
+      const game = await gameRepository.get(req.params.gameId)
+      const openCells = game.field.points(cell => cell.isOpen)
+      res.status(200).json(openCells)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  /**
+   * セルを開く（開いたセルを作る）
+   */
+  router.post('/:gameId/cells/open', async function (req, res, next) {
+    try {
+      const game = await gameRepository.get(req.params.gameId)
+      const point = { x: req.body.x, y: req.body.y }
+
+      if (game.field.cellAt(point).isOpen) {
+        const openCells = game.field.points(cell => cell.isOpen)
+        res.status(200).json(openCells)
+        return
+      }
+
+      game.open(point.x, point.y)
+      await gameRepository.update(game)
+
+      const openCells = game.field.points(cell => cell.isOpen)
+      res.status(201).json(openCells)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  /**
+   * 開いたセルを閉じる
+   */
+  router.delete('/:gameId/cells/open/:id', async function (req, res, next) {
+    res.status(405).end()
+  })
+}
