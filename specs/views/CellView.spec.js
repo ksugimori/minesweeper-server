5c56206dbbd6fda4051f047c897e656ab51cfdaf
ksugimori <ksugimori.private@gmail.com>
2021-05-01 22:59:21 +0900

    open-cells のレスポンスに count を追加

---
+++ specs/views/CellView.spec.js
@@ -0,0 +1,17 @@
+const Cell = require('../../lib/models/Cell.js')
+const Point = require('../../lib/models/util/Point.js')
+const CellView = require('../../lib/views/CellView.js')
+
+describe('CellView', () => {
+  describe('#toJSON', () => {
+    test('x, y, count が反映されること', () => {
+      const point = Point.of(1, 2)
+      const cell = new Cell({ count: 8 })
+
+      const view = new CellView(point, cell)
+      expect(view.toJSON().x).toBe(1)
+      expect(view.toJSON().y).toBe(2)
+      expect(view.toJSON().count).toBe(8)
+    })
+  })
+})
