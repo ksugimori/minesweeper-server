3ace95cd82b666d989eb5b9457037eff03eeec3f
ksugimori <ksugimori.private@gmail.com>
2021-05-08 21:49:12 +0900

    リポジトリクラスをシングルトンではなくす

--- specs/api/GameController.spec.js
+++ specs/api/GameController.spec.js
@@ -2,17 +2,19 @@ const request = require('supertest')
 const app = require('../../app.js')
 const Game = require('../../lib/models/Game')
 const DataNotFoundException = require('../../lib/exceptions/DataNotFoundException')
-const gameRepository = require('../../lib/repositories/GameRepository')
+const GameRepository = require('../../lib/repositories/GameRepository')
 jest.mock('../../lib/repositories/GameRepository')
-const mockUtils = require('../utils/mockUtils')
-const Point = require('../../lib/models/util/Point')
 
 describe('POST /api/games', () => {
   test('Game オブジェクトが返されること', async () => {
     // INSERT 時に ID が割り当てられる挙動をモックで再現
-    gameRepository.create = jest.fn(arg => {
-      arg.id = 999
-      return arg
+    GameRepository.mockImplementation(() => {
+      return {
+        create: arg => {
+          arg.id = 999
+          return arg
+        }
+      }
     })
 
     const req = { width: 3, height: 2, numMines: 1 }
@@ -52,7 +54,11 @@ describe('GET /api/games/{id}', () => {
   test('取得できた場合はステータス 200 が返ること', async () => {
     const game = new Game()
     game.id = 999
-    gameRepository.get = jest.fn(() => game)
+    GameRepository.mockImplementation(() => {
+      return {
+        get: () => game
+      }
+    })
 
     const response = await request(app).get('/api/games/1')
 
@@ -61,8 +67,12 @@ describe('GET /api/games/{id}', () => {
   })
 
   test('取得できなかった場合はステータス 404 が返ること', async () => {
-    gameRepository.get = jest.fn(() => {
-      throw new DataNotFoundException()
+    GameRepository.mockImplementation(() => {
+      return {
+        get: () => {
+          throw new DataNotFoundException()
+        }
+      }
     })
 
     const response = await request(app).get('/api/games/1')
