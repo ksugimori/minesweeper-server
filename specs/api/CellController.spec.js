c2c4f2bb12026f9c2fd8fc5f3bcd0b6c5d1e2d48
ksugimori <ksugimori.private@gmail.com>
2021-05-08 21:02:15 +0900

    ファイル名変更

--- specs/api/CellController.spec.js
+++ specs/api/CellController.spec.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
 const app = require('../../app.js')
 const Point = require('../../lib/models/util/Point')
-const gameRepository = require('../../lib/repositories/gameRepository')
-jest.mock('../../lib/repositories/gameRepository')
+const gameRepository = require('../../lib/repositories/GameRepository')
+jest.mock('../../lib/repositories/GameRepository')
 const mockUtils = require('../utils/mockUtils')
 
 describe('GET /api/games/:gameId/open-cells', () => {
